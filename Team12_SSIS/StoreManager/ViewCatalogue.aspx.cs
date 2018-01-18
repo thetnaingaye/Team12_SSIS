b70e67e2be31d7857c1d23ea8be61de7848d915b
Yishu <monicayuan95@gmail.com>
2018-01-18 23:17:48 +0800

    View Catalog

--- Team12_SSIS/StoreManager/ViewCatalogue.aspx.cs
+++ Team12_SSIS/StoreManager/ViewCatalogue.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Team12_SSIS.Model;
 
 namespace Team12_SSIS.StoreManager
 {
@@ -11,7 +12,19 @@ namespace Team12_SSIS.StoreManager
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
 
+        protected void BindGrid()
+        {
+            using (SA45Team12AD entities = new SA45Team12AD())
+            {
+                GridView1.DataSource = entities.InventoryCatalogues.ToList<InventoryCatalogue>();
+                GridView1.DataBind();
+            }
         }
     }
 }
