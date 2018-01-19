52bbd02d7a3eac597d56e017595b95b916f0f29e
Pradeep Elango <pradeep1991@gmail.com>
2018-01-19 16:28:58 +0800

    Added UserLogic.cs

---
+++ Team12_SSIS/BusinessLogic/UserLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team12_SSIS.BusinessLogic
+{
+	public class UserLogic
+	{
+		public static string GetCurrentDep()
+		{
+			return HttpContext.Current.Profile.GetPropertyValue("department").ToString();
+		}
+
+
+
+	}
+}
