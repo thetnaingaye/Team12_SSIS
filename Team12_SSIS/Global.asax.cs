575f9ebf7eb791cbce023540b7c59ec054681253
thetnaingaye <mmu1tn@gmail.com>
2018-01-23 11:12:30 +0800

    added threading for time scheduler

--- Team12_SSIS/Global.asax.cs
+++ Team12_SSIS/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -12,7 +13,28 @@ namespace Team12_SSIS
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            Application["count"] = 0;
+            Thread thread = new Thread(new ThreadStart(ThreadFunc));
+            thread.IsBackground = true;
+            thread.Name = "ThreadFunc";
+            thread.Start();
 
+        }
+        protected void ThreadFunc()
+        {
+            System.Timers.Timer t = new System.Timers.Timer();
+            t.Elapsed += new System.Timers.ElapsedEventHandler(TimerWorker);
+            t.Interval = 5000;
+            t.Enabled = true;
+            t.AutoReset = true;
+            t.Start();
+        }
+        protected void TimerWorker(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            int count = Convert.ToInt32(Application["count"]);
+
+            count++;
+            Application["count"] = count;
         }
 
         protected void Session_Start(object sender, EventArgs e)
