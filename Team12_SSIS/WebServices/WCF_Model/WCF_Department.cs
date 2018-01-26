b048832b4a0bb8bdacf524d1460e4ceebcb91aa8
thetnaingaye <mmu1tn@gmail.com>
2018-01-27 00:11:16 +0800

    add WCF_Department

---
+++ Team12_SSIS/WebServices/WCF_Model/WCF_Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Team12_SSIS.WebServices.WCF_Model
+{
+    [DataContract]
+    public class WCF_Department
+    {
+        [DataMember]
+        public string DeptID { get; set; }
+
+        [DataMember]
+        public string DepartmentName { get; set; }
+    }
+}
