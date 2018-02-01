76f92104cb9ad5ee16a82fb104e21b181bcaf815
hikari8807 <checkingmymail@gmail.com>
2018-02-01 19:23:12 +0800

    Minor updated

--- Team12_SSIS/DepartmentHead/ManageDelegation.aspx.cs
+++ Team12_SSIS/DepartmentHead/ManageDelegation.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Author - Pradeep Elango
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
--- Team12_SSIS/StoreClerk/ChangeOrderLeadTime.aspx.cs
+++ Team12_SSIS/StoreClerk/ChangeOrderLeadTime.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Author - Pradeep Elango 
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
